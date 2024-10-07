6effb47038b49acefae3bea0d9a6df5d8193968a
Adsyarif <adrisansyarif@gmail.com>
2024-10-07 11:53:30 +0700

    fix: atribut svg stroke-widh to strokeWidh in login page

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -36,7 +36,7 @@ const LoginPage: React.FC = () => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="size-6"
             onClick={handleBack}
