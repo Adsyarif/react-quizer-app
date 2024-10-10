532a0e7cf34c93bc3ec636575205fba4123ec09d
Adrisa <adrisansyarif@gmail.com>
2024-10-10 23:17:25 +0700

    feat: Logo Quizer redirect to home page

--- src/components/common/Navigation/index.tsx
+++ src/components/common/Navigation/index.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const Navigation = () => {
+  const navigate = useNavigate();
   return (
     <nav className="flex justify-between items-center p-6 md:px-12">
-      <div className="text-white text-2xl font-semibold">Quizer</div>
+      <div
+        className="text-white text-2xl font-semibold"
+        onClick={() => navigate("/")}
+      >
+        Quizer
+      </div>
       <div className="hidden md:flex space-x-6 text-gray-300">
         <button className="hover:text-white transition-colors">Category</button>
         <button className="hover:text-white transition-colors">
